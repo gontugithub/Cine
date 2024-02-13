b0f193c39a685b6121693325cac105b7bac02bad
Gonzalo <gs.alvarez8@gmail.com>
2024-02-13 13:41:22 +0100

    feat(src/main): main main structure created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,37 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+
+
+
+        do {
+
+            System.out.print("CINE\n 1 VER LAS SALAS Y DATOS DE SUS PEL\n 2 ELEGIR SALA Y BUTACA PARA RESERVAR\n 3 VER MATRIZ DE ASIENTOS DE X SALA\n >> ");
+            opcion =  sc.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("HOLA");
+                    break;
+
+                case 2:
+                    System.out.println("ADIOS");
+                    break;
+
+                case 3:
+                    System.out.println("BUENAS");
+                    break;
+            }
+        } while (opcion != 10);
+
+
 
     }
 }
