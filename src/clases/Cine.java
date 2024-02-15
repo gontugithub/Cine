8b9166867727674bc9aa89b225f9e920ec0e2a30
Gonzalo <gs.alvarez8@gmail.com>
2024-02-15 09:25:39 +0100

    feat(src/clases): cine constructor salas array fill

--- src/clases/Cine.java
+++ src/clases/Cine.java
@@ -6,9 +6,16 @@ public class Cine {
     private int aforo;
     private Sala[] salas;
 
-    public Cine(String nombre, int aforo, int numerosalas) {
+    public Cine(String nombre, int aforo, int numerosalas, int filas, int columnas) {
         this.nombre = nombre;
         this.aforo = aforo;
         salas = new Sala[numerosalas];
+        
+        for (int i = 0; i<salas.length;i++) {
+        	
+        	// Añadimos los datos de cada sala solo faltaria añadirle la pelicula, que lo haremos en el main
+        	
+        	salas[i] = new Sala(i, filas, columnas, null);
+        }
     }
 }
--- src/clases/Sala.java
+++ src/clases/Sala.java
@@ -5,7 +5,7 @@ public class Sala {
     private int numeroid;
     private int[][] butacas;
 
-    public Sala(int numeroid,int filas, int columnas) {
+    public Sala(int numeroid,int filas, int columnas, Pelicula Pelicula) {
         this.numeroid = numeroid;
         butacas = new int[filas][columnas];
     }
