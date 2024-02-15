ad7def6dcb27d88a91b651ed5f6ae94e5a3c3dfa
Gonzalo <gs.alvarez8@gmail.com>
2024-02-15 09:31:58 +0100

    feat(src/sala.java): sala getters and setters

--- src/clases/Sala.java
+++ src/clases/Sala.java
@@ -4,9 +4,37 @@ public class Sala {
 
     private int numeroid;
     private int[][] butacas;
+    private Pelicula pelicula;
 
     public Sala(int numeroid,int filas, int columnas, Pelicula Pelicula) {
         this.numeroid = numeroid;
         butacas = new int[filas][columnas];
+        this.pelicula = Pelicula;
     }
+
+	public int getNumeroid() {
+		return numeroid;
+	}
+
+	public void setNumeroid(int numeroid) {
+		this.numeroid = numeroid;
+	}
+
+	public int[][] getButacas() {
+		return butacas;
+	}
+
+	public void setButacas(int[][] butacas) {
+		this.butacas = butacas;
+	}
+
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+    
+    
 }
